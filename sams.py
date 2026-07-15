e647b926222dcb9bbe68ede83edafe8cef2ee0d7
lahiruDilhara <galahirudilhara@gmail.com>
2026-07-15 17:05:17 +0530

    added the command parser

--- sams.py
+++ sams.py
@@ -1,4 +1,31 @@
-from pytesseract import pytesseract
 import cv2
 import numpy as np
+import argparse
+from rich_argparse import RichHelpFormatter
+from pathlib import Path
 
+def parser():
+    parser = argparse.ArgumentParser(
+        description="Student Attendance Management System",
+        formatter_class=RichHelpFormatter,
+        usage="python3 sams.py [-h] <image_file> <xml_file>"
+    )
+
+    parser.add_argument("image",type=Path,help="Path to the image file")
+    parser.add_argument("xml",type=Path,help="Path to the student xml file")
+
+    args = parser.parse_args()
+    return args.image, args.xml
+
+
+def main():
+    
+    (imagePath, xmlPath) = parser()
+    print(imagePath)
+    print(xmlPath)
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
